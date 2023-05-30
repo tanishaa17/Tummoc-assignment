ca4b3a57dc283747de6d20bca453841e88279365
tanishaa17 <krati1701@gmail.com>
2023-05-31 00:57:43 +0530

    navbar added

--- user-authentication-app/src/Components/Navbar/Navbar.jsx
+++ user-authentication-app/src/Components/Navbar/Navbar.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import "../Navbar/Navbar.css"
 
 export const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
+    return (
+        <div id='navbar'>
+            <img src="https://tummoc.com/images/news/financialexpress-tummoc.png" alt="tummoc-logo" />
+
+            <div id='loginDiv'>
+                <p>Login</p>
+                <p>Register</p>
+            </div>
+        </div>
+    )
 }
