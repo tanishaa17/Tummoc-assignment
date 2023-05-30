96040e418e290497439b2ce1bf02e3dd11214074
tanishaa17 <krati1701@gmail.com>
2023-05-31 00:59:19 +0530

    login/signup basic structure created

--- user-authentication-app/src/Components/Login/Login.jsx
+++ user-authentication-app/src/Components/Login/Login.jsx
@@ -1,7 +1,39 @@
-import React from 'react'
-
+import React, { useState } from "react";
+import '../Login/Login.css'
 export const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   return (
-    <div>Login</div>
+    <form id="loginForm">
+      <h2 >Login Page</h2>
+
+      <div>
+        <label>
+          Email:
+        </label>
+        <input type="email" placeholder="Enter email" required value={email} onChange={(e) => { setEmail(e.target.value) }} />
+      </div>
+      <div >
+        <label >
+          Password:
+        </label>
+        <input type="password" placeholder="Password" required value={password} onChange={(e) => { setPassword(e.target.value) }} />
+      </div>
+
+      <div>
+        <label>
+          <input type="checkbox" />
+          <span >
+            Remember me
+          </span>
+        </label>
+      </div>
+
+      <div>
+        <button type="button">
+          LOGIN
+        </button>
+      </div>
+    </form>
   )
-}
+};
--- user-authentication-app/src/Components/Register/Register.jsx
+++ user-authentication-app/src/Components/Register/Register.jsx
@@ -1,7 +1,68 @@
-import React from 'react'
+import React, { useState } from "react";
+import '../Register/Register.css'
 
 export const Register = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const register = () => {
+    if (password !== confirmPassword) {
+      alert(`Password doesn't match`)
+    } else {
+      const userData = {
+        name,
+        email,
+        password
+      }
+      console.log(userData);
+    }
+  }
   return (
-    <div>Register</div>
+    <form id="registerForm">
+      <h2>Register Page</h2>
+      <div>
+        <label >
+          Full Name:
+        </label>
+        <input type="text" placeholder="Enter Name" required value={name} onChange={(e) => { setName(e.target.value) }} />
+      </div>
+
+      <div>
+        <label>
+          Email:
+        </label>
+        <input type="email" placeholder="Enter email" required value={email} onChange={(e) => { setEmail(e.target.value) }} />
+      </div>
+
+      <div >
+        <label >
+          Password:
+        </label>
+        <input type="password" placeholder="Password" required value={password} onChange={(e) => { setPassword(e.target.value) }} />
+      </div>
+
+      <div>
+        <label>
+          Confirm password:
+        </label>
+        <input type="password" placeholder="Confirm password" required value={confirmPassword} onChange={(e) => { setConfirmPassword(e.target.value) }} />
+      </div >
+
+      <div >
+        <label>
+          <input type="checkbox" />
+          <span >
+            Remember me
+          </span>
+        </label>
+      </div>
+      <div>
+        <button type="button" onClick={register}>
+          REGISTER
+        </button>
+      </div>
+    </form >
   )
-}
+};
