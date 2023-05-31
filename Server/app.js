31cc2d4a94ddf164fbe255494bbbc9fb58964238
tanishaa17 <krati1701@gmail.com>
2023-05-31 12:24:36 +0530

    fixing server errors

---
+++ Server/app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const { connection } = require("./configs/db");
+const app = express();
+require("dotenv").config();
+app.use();
+
+const port = process.env.REACT_APP_PORT || 3000;
+
+app.listen(port, async () => {
+    try {
+        await connection
+        console.log(`Successfully connected to DB`);
+    } catch (error) {
+        console.log(`Error while connecting DB : ${error}`);
+    }
+    console.log(`Server is running on ${port}`);
+})
